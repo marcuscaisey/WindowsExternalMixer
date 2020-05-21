f82e28af3f3b9dd18474f895075e27ce059e8f6f
Marcus Caisey <34950778+marcuscaisey@users.noreply.github.com>
2020-05-21 17:37:49 +0100

    Add basic sketch using the Rotary class

---
+++ arduino/src/main.cpp
@@ -0,0 +1,33 @@
+#include "Arduino.h"
+#include "rotary.h"
+
+using rotary::Rotary;
+
+#define CLK 2
+#define DT 3
+
+Rotary rotary1 = Rotary(CLK, DT);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("setup begin");
+
+  attachInterrupt(digitalPinToInterrupt(CLK), isr, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT), isr, CHANGE);
+
+  Serial.println("setup finish");
+}
+
+void loop() {}
+
+void isr() {
+  Rotary::Direction direction = rotary.processInputs();
+  switch (direction) {
+    case Rotary::Direction::CLOCKWISE:
+      Serial.println("u");
+      break;
+    case Rotary::Direction::ANTI_CLOCKWISE:
+      Serial.println("d");
+      break;
+  }
+}
